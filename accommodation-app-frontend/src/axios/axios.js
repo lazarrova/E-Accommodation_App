bc7799283ac4ce521f317e57dd826abdfdc5786e
lazarrova <vaska.lazarova1@gmail.com>
2025-07-01 00:30:37 +0200

    changed axios

--- accommodation-app-frontend/src/axios/axios.js
+++ accommodation-app-frontend/src/axios/axios.js
@@ -1,31 +1,27 @@
-
-
 import axios from "axios";
+// const baseURL = import.meta.env.MODE === 'development'
+//     ? 'http://localhost:8181'
+//     : '/api';
+let baseURL = "";
 
-const baseURL =
-    import.meta.env.MODE === 'development'
-        ? 'http://localhost:9090/api' // docker-compose
-        : 'http://backend.local/api'; // kubernetes ingress
+const hostname = window.location.hostname;
+
+if (hostname === "localhost") {
+    baseURL = "http://localhost:8181";
+} else if (hostname === "backend.local") {
+    baseURL = "/api";
+} else {
+    baseURL = "http://backend.local/api"; // или остави нешто како default ако треба
+}
 
 const axiosInstance = axios.create({
-    baseURL: baseURL,
+    //baseURL: 'http://localhost:8181, //with docker-compose
+    //baseURL: "/api", //with kubernetes
+    //baseURL: "https://accommodation-backend.onrender.com", //with deployment
+    baseURL,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
 export default axiosInstance;
-
-
-
-// import axios from "axios";
-//
-// const axiosInstance = axios.create({
-//
-//     baseURL: "http://localhost:9090/api",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-// });
-//
-// export default axiosInstance;
