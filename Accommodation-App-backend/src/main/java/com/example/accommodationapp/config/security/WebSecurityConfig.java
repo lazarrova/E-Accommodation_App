6472a7d83d44319beea872a65d5a3412f8594143
lazarrova <vaska.lazarova1@gmail.com>
2025-06-30 18:22:48 +0200

    cors fix

--- Accommodation-App-backend/src/main/java/com/example/accommodationapp/config/security/WebSecurityConfig.java
+++ Accommodation-App-backend/src/main/java/com/example/accommodationapp/config/security/WebSecurityConfig.java
@@ -33,17 +33,18 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                "http://localhost:3000",     // за локално девелопирање
-                "http://frontend.local"      // за production/K8s ingress
-        ));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://frontend.local"));
+        corsConfiguration.disable();
+//        corsConfiguration.setAllowedOrigins(List.of(
+//                "http://localhost:3000",     // за локално девелопирање
+//                "http://frontend.local"      // за production/K8s ingress
+//        ));
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://frontend.local"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("*")); //
+//        corsConfiguration.setAllowedOrigins(List.of("*")); //
         return source;
     }
 
